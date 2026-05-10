124f497f474f8fa924b004f6dbf259afd6c6857d
mfl4 <91680736+mfl4@users.noreply.github.com>
2026-05-10 13:02:34 +0700

    chore[frontend]: initialize React Vite + Tailwind
    Initialize frontend project structure (react-vite with Vite+React+Tailwind) and static example (react-html).

---
+++ react-vite/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-400">
+      <h1 className="text-4xl font-bold text-white">Hello Han</h1>
+    </div>
+  );
+}
+
+export default App;
